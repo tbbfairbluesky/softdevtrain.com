f97b926857effab3a6601f7fe967f0fb985d427a
Timothy B. Brown <tbbrown@wustl.edu>
2018-03-14 17:12:16 -0500

    added a post

--- conf.py
+++ conf.py
@@ -17,8 +17,8 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Software Development Training, LLC"  # (translatable)
-BLOG_TITLE = "Software Development Training, LLC"  # (translatable)
+BLOG_AUTHOR = "Software Development Training LLC"  # (translatable)
+BLOG_TITLE = "Software Development Training LLC"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "http://www.SoftDevTrain.com/"
